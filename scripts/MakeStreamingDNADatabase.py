4aab84d0b51ea33032f7db3c6ab1342e1a96a6c7
dkoslicki <david.koslicki@math.oregonstate.edu>
2017-10-23 12:06:59 -0700

    created script to make database for streaming queries

--- scripts/MakeStreamingDNADatabase.py
+++ scripts/MakeStreamingDNADatabase.py
@@ -16,7 +16,7 @@ import multiprocessing
 from itertools import *
 import argparse
 import khmer
-from khmer.khmer_args import optimal_size
+import tst
 
 # This function will make a single min hash sketch upon being given a file name, sketch size, prime, and k-mer size
 def make_minhash(genome, max_h, prime, ksize):
@@ -40,10 +40,10 @@ def main():
 	parser.add_argument('-p', '--prime', help='Prime (for modding hashes)', default=9999999999971)
 	parser.add_argument('-t', '--threads', type=int, help="Number of threads to use", default=multiprocessing.cpu_count())
 	parser.add_argument('-n', '--num_hashes', type=int, help="Number of hashes to use.", default=500)
-	parser.add_argument('-k', '--k_size', type=int, help="K-mer size", default=21)
-	parser.add_argument('-i', '--intersect_nodegraph', action="store_true",
-						help="Optional flag to export Nodegraph file (bloom filter) containing all k-mers in the"
-							 " training database. Saved in same location as out_file. This is to be used with QueryDNADatabase.py")
+	parser.add_argument('-k', '--k_size', type=int, help="k-mer size", default=21)
+	parser.add_argument('-s', '--streaming_database', action="store_true",
+						help="Optional flag to export a ternary search tree (tst file) in the same location as out_file."
+							 "This can then be used with StreamingQueryDNADatabase.py")
 	parser.add_argument('in_file', help="Input file: file containing (absolute) file names of training genomes.")
 	parser.add_argument('out_file', help='Output training database/reference file (in HDF5 format)')
 	args = parser.parse_args()
@@ -55,10 +55,13 @@ def main():
 	if not os.path.exists(input_file_names):
 		raise Exception("Input file %s does not exist." % input_file_names)
 	out_file = os.path.abspath(args.out_file)
-	if args.intersect_nodegraph is True:
-		intersect_nodegraph_file = os.path.splitext(out_file)[0] + ".intersect.Nodegraph"
+
+	# check for and make filename for tst file
+	if args.streaming_database is True:
+		streaming_database_file = os.path.splitext(out_file)[0] + ".tst"
+		streaming_database_file = os.path.abspath(streaming_database_file)
 	else:
-		intersect_nodegraph_file = None
+		streaming_database_file = None
 
 	file_names = list()
 	fid = open(input_file_names, 'r')
@@ -76,17 +79,15 @@ def main():
 	# Export all the sketches
 	MH.export_multiple_to_single_hdf5(genome_sketches, out_file)
 
-	# If requested, save all the k-mers into a big Nodegraph (unfortunately, need to pass through the data again since we
-	# a-priori don't know how big of a table we need to make
-	if intersect_nodegraph_file is not None:
-		total_num_kmers = 0
-		for sketch in genome_sketches:
-			total_num_kmers += sketch._true_num_kmers
-		res = optimal_size(total_num_kmers, fp_rate=0.001)
-		intersect_nodegraph = khmer.Nodegraph(ksize, res.htable_size, res.num_htables)
-		for file_name in file_names:
-			intersect_nodegraph.consume_seqfile(file_name)
-		intersect_nodegraph.save(intersect_nodegraph_file)
+	# If requested, save the ternary search tree
+	if streaming_database_file is not None:
+		tree = tst.TST()  # tst array
+		for i in range(len(genome_sketches)):
+			for kmer_index in range(len(genome_sketches[i]._kmers)):
+				kmer = genome_sketches[i]._kmers[kmer_index]
+				tree[kmer + 'x' + str(i) + 'x' + str(kmer_index)] = True  # format here is kmer+x+hash_index+kmer_index
+		tree.write_to_file(streaming_database_file)
+
 
 if __name__ == "__main__":
 	main()
