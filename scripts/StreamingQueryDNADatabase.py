Fixed issue with over-counting kmers. Also reduced memory footprint by a factor of num_hashes (so ~1000x)

scripts/StreamingQueryDNADatabase.py
@@ -137,19 +137,20 @@ if __name__ == '__main__':
 		def __init__(self):
 			self.ksize = max_ksize
 			N = len(sketches)
-			to_share = multiprocessing.Array(ctypes.c_bool, N * num_hashes * len(k_range))  # vector
-			to_share_np = np.frombuffer(to_share.get_obj(), dtype=ctypes.c_bool)  # get it
-			self.vals = to_share_np.reshape(N, num_hashes, len(k_range))
+			to_share = multiprocessing.Array(ctypes.c_int64, N * len(k_range))  # vector
+			to_share_np = np.frombuffer(to_share.get_obj(), dtype=ctypes.c_int64)  # get it
+			self.vals = to_share_np.reshape(N, len(k_range))
 			self.lock = Lock()  # don't need since I don't care about collisions (once matched, always matched)
 
-		def increment(self, hash_index, kmer_index, k_size_loc):
-			self.vals[hash_index, kmer_index, k_size_loc] = True  # array
+		def increment(self, hash_index, k_size_loc):
+			self.vals[hash_index, k_size_loc] += 1  # array
 
 		def value(self, hash_index, k_size_loc):
-			return np.sum(self.vals[hash_index, :, k_size_loc])  # get the number of True's
+			return self.vals[hash_index, k_size_loc]
+			#return np.sum(self.vals[hash_index, :, k_size_loc])  # get the number of True's
 
-		def value_at_kmer(self, hash_index, kmer_index, k_size_loc):
-			return self.vals[hash_index, kmer_index, k_size_loc]
+		#def value_at_kmer(self, hash_index, kmer_index, k_size_loc):
+		#	return self.vals[hash_index, kmer_index, k_size_loc]
 
 		def process_seq(self, seq):
 			for k_size_loc in range(len(k_range)):  # could do this more efficiently by putting this in the inner loop
@@ -159,15 +160,16 @@ if __name__ == '__main__':
 					if kmer in all_kmers_set:
 						prefix_matches = tree.keys(kmer)  # get all the k-mers whose prefix matches
 						all_kmers_set.remove(kmer)  # Drop from the pre-filter list, since it will subsequently not be used
-						hash_loc_kmer_loc = []
+						hash_to_increment = []
 						# get the location of the found kmers in the counters
 						for item in prefix_matches:
 							split_string = item.split('x')
-							hash_loc_kmer_loc.append((int(split_string[1]), int(
-								split_string[2])))  # first is the hash location, second is which k-mer
-						if len(hash_loc_kmer_loc) > 0:
-							for hash_index, kmer_index in hash_loc_kmer_loc:
-								self.increment(hash_index, kmer_index, k_size_loc)
+							hash_to_increment.append(int(split_string[1]))  # first is the hash location, second is which k-mer
+						# uniquify so I don't over count
+						hash_to_increment = set(hash_to_increment)
+						if hash_to_increment:
+							for hash_index in hash_to_increment:
+								self.increment(hash_index, k_size_loc)
 
 	# helper function
 	def q_func(queue, counter):
@@ -223,11 +225,12 @@ if __name__ == '__main__':
 				kmer = sketches[sketch_index]._kmers[index][0:k_range[k_size_loc]]  # get the first few characters of the kmer
 				if kmer not in uniqe_kmers:
 					uniqe_kmers.add(kmer)
-					uniqe_kmer_indicies.add(index)
+					#uniqe_kmer_indicies.add(index)
 			# get the total counts for these unique k-mer indices
-			count = 0
-			for index in uniqe_kmer_indicies:
-				count += counter.value_at_kmer(sketch_index, index, k_size_loc)
+			#count = 0
+			#for index in uniqe_kmer_indicies:
+			#	count += counter.value_at_kmer(sketch_index, index, k_size_loc)
+			count = counter.value(sketch_index, k_size_loc)
 			# update the containment index
 			containment_indices[sketch_index, k_size_loc] = count / float(len(uniqe_kmers))  # this is the containment index estimate
 			#intersection_counts[sketch_index, k_size_loc] = counter.value(sketch_index, k_size_loc)  # this old way contained duplicate counts