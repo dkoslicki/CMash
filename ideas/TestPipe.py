00c3a71edc3fd50a3f98ed61afc1544c7aede786
dkoslicki <david.koslicki@math.oregonstate.edu>
2018-08-17 10:57:05 -0700

    Add test script to explore using pipes instead of queues

---
+++ ideas/TestPipe.py
@@ -0,0 +1,25 @@
+import multiprocessing
+
+def worker(procnum, send_end):
+    '''worker function'''
+    result = str(procnum) + ' represent!'
+    print result
+    send_end.send(result)
+
+def main():
+    jobs = []
+    pipe_list = []
+    for i in range(5):
+        recv_end, send_end = multiprocessing.Pipe(False)
+        p = multiprocessing.Process(target=worker, args=(i, send_end))
+        jobs.append(p)
+        pipe_list.append(recv_end)
+        p.start()
+
+    for proc in jobs:
+        proc.join()
+    result_list = [x.recv() for x in pipe_list]
+    print result_list
+
+if __name__ == '__main__':
+    main()
