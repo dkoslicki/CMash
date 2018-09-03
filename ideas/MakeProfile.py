8567a96e6c6bda67ba9cfe80064d9e3d6ac439a1
dkoslicki <david.koslicki@math.oregonstate.edu>
2018-09-03 10:31:07 -0700

    Change rel abund so things don't zero out

--- ideas/MakeProfile.py
+++ ideas/MakeProfile.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
 	parser.add_argument('-o', '--out_file', type=str, help="File name of output *.profile file")
 	parser.add_argument('--threshold', type=float, help="Threshold for which to include results in profile file", default=0)
 	parser.add_argument('--key', type=str, help="Which k-mer size to do the thresholding. eg. 'k=25'. Default is largest k-mer size", default=False)
+	parser.add_argument('--sample_id', type=str, help="Sample ID", default="")
 
 	# read in the arguments
 	args = parser.parse_args()
@@ -23,10 +24,10 @@ if __name__ == '__main__':
 	profile_out_file = args.out_file  # '/home/dkoslicki/Data/MiCOPMinHash/Test/Test.profile'
 	coverage_threshold = args.threshold
 	sort_key = args.key
-	sample_id = os.path.abspath(cmash_out_file)
+	sample_id = args.sample_id  #os.path.abspath(cmash_out_file)
 
 	abbrv_to_rank = dict()
-	abbrv_to_rank['k'] = "kingdom"
+	abbrv_to_rank['k'] = "superkingdom"
 	abbrv_to_rank['p'] = "phylum"
 	abbrv_to_rank['c'] = "class"
 	abbrv_to_rank['o'] = "order"
@@ -73,12 +74,15 @@ if __name__ == '__main__':
 				tax_info_to_rank = tax_info[2].split('|')[:rank_ind]  # tax info up to the rank under consideration
 				tax_id = [i.split('_')[2] for i in tax_info_to_rank][-1]  # get the last guy's tax id
 				rank = abbrv_to_rank[tax_info_to_rank[-1].split('_')[0]]  # get the last guy's rank
+				tax_path_list = [i.split('_')[2] for i in tax_info_to_rank]
+				if len(tax_path_list) != len(set(tax_path_list)):
+					continue
 				tax_path = '|'.join([i.split('_')[2] for i in tax_info_to_rank])  # join up the tax path
 				tax_path_sn = '|'.join([' '.join(i.split('_')[3:]) for i in tax_info_to_rank])  # join up the tax path names
-				if tax_id in tax_id_to_abund:  # properly account for abundances
-					tax_id_to_abund[tax_id] += 1
+				if tax_id in tax_id_to_abund:  # Don't care about the abundances
+					tax_id_to_abund[tax_id] = .01  # so don't increment this
 				else:
-					tax_id_to_abund[tax_id] = 1
+					tax_id_to_abund[tax_id] = .01
 				percentage = tax_id_to_abund[tax_id]
 			# this was the old way when I was just adding species
 			#tax_id = tax_info[1]
@@ -91,3 +95,11 @@ if __name__ == '__main__':
 			print("uh oh, this file isn't in the taxonomy: %s" % name)
 			sys.exit(1)
 	fid.close()
+
+# then use something like the following:
+# import ProfilingTools as PF
+# profile=PF.Profile('/home/dkoslicki/Data/MiCOPMinHash/Test/RL_S001__insert_270_classified.profile')
+# profile.normalize()
+# profile._populate_missing_dont_use()
+# profile.normalize()
+# profile.write_file('/home/dkoslicki/Data/MiCOPMinHash/Test/RL_S001__insert_270_classified.profile.norm')
