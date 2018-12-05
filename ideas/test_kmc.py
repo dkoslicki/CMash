580747feacdabc094f3d234444daba0374ea7409
dkoslicki <dmkoslicki@gmail.com>
2018-12-04 17:52:12 -0800

    add more of the script in

--- ideas/test_kmc.py
+++ ideas/test_kmc.py
@@ -22,7 +22,7 @@ with open(training_file_names, 'r') as fid:
 		file_names.append(os.path.basename(line))
 
 print("importing kmers")
-chunk_size = 1000
+chunk_size = 5000
 iter = 0
 with open(training_out_file, 'w') as fid:
 	for file_iter in xrange(0, len(file_names), chunk_size):
@@ -39,3 +39,28 @@ with open(training_out_file, 'w') as fid:
 			iter += 1
 		fid.write(to_write)
 
+###########################################################################################
+# Next, run kmc on this thing
+# with a bash script, use the following:
+# /home/pi/koslickd/KMC/./kmc -v -k60 -m200 -sm -fm -ci0 -cs3 -t48 -jlog_train NathanRefSeqTraining60mers.fa NathanRefSeq60mers /scratch/kmc_temp/
+#
+
+############################################################################################
+# Next, run kmc on the input file
+# in bash:
+# /home/pi/koslickd/KMC/./kmc -v -k60 -m200 -sm -fq -ci2 -cs3 -t48 -jlog_sample /nfs1/Koslicki_Lab/koslickd/MiCOPCMash/CAMITest/RL_S001__insert_270.fq.gz CAMILow60mers /scratch/kmc_temp/
+# 7 min, 38 seconds
+
+#############################################################################################
+# Next, intersect them
+# in bash:
+# /home/pi/koslickd/KMC/./kmc_tools simple NathanRefSeq60mers CAMILow60mers intersect intersection
+
+##############################################################################################
+# Dump them and turn into a fasta file
+# /home/pi/koslickd/KMC/./kmc_dump intersection intersection_dump
+# cat intersection_dump | cut -f1 | sed 's/^/>seq\n/g' > intersection_dump.fa
+
+#############################################################################################
+# Then run CMash on intersection_dump.fa
+#/nfs1/Koslicki_Lab/koslickd/MiCOPCMash/CMashVE/bin/python /nfs1/Koslicki_Lab/koslickd/MiCOPCMash/CMash/scripts/StreamingQueryDNADatabase.py /nfs1/Koslicki_Lab/koslickd/KMC_test/intersection_dump.fa /nfs1/Koslicki_Lab/koslickd/MiCOPCMash/TrainingData/NathanRefSeq/micopdb_n_1000_k_60.h5 /nfs1/Koslicki_Lab/koslickd/KMC_test/RL_S001__insert_270_classified_kmc.csv 30-60-10 -c 0 -r 1000000 -v -f /nfs1/Koslicki_Lab/koslickd/MiCOPCMash/TrainingData/NathanRefSeq/micopdb_n_1000_k_60_prefilter_30_60_10.bf
