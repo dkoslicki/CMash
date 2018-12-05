ccdb90bf0b675bb437ecec182bdec16e208b3bbd
dkoslicki <dmkoslicki@gmail.com>
2018-12-04 17:24:41 -0800

    Much faster dump

--- ideas/test_kmc.py
+++ ideas/test_kmc.py
@@ -8,6 +8,7 @@
 from CMash import MinHash as MH
 import os
 import blist
+import itertools
 
 training_out_file = '/nfs1/Koslicki_Lab/koslickd/KMC_test/NathanRefSeqTraining60mers.fa'
 training_data ='/nfs1/Koslicki_Lab/koslickd/MiCOPCMash/TrainingData/NathanRefSeq/micopdb_n_1000_k_60.h5'
@@ -21,28 +22,20 @@ with open(training_file_names, 'r') as fid:
 		file_names.append(os.path.basename(line))
 
 print("importing kmers")
-all_kmers = blist.blist()
+chunk_size = 1000
 iter = 0
-for file_name in file_names:
-	iter += 1
-	sketch = MH.import_multiple_from_single_hdf5(training_data, import_list=[file_name])[0]
-	all_kmers += sketch._kmers
-	if iter % 1000 == 0:
-		print("Finished %d files" % iter)
-
-print("forming the set")
-all_kmers_set = set(all_kmers)
-
-num_kmers = len(all_kmers_set)
-print("exporting results")
 with open(training_out_file, 'w') as fid:
-	iter = 0
-	for kmer in all_kmers_set:
-		fid.write(">seq_%d\n" % iter)
-		fid.write("%s\n" % kmer)
-		iter += 1
-		if iter % 1000000 == 0:
-			print("On kmer %d of %d" % (iter, num_kmers))
-
-##########################################################################
+	for file_iter in xrange(0, len(file_names), chunk_size):
+		print("on file: %d" % file_iter)
+		file_names_subset = file_names[file_iter:file_iter+chunk_size]
+		sketchs = MH.import_multiple_from_single_hdf5(training_data, import_list=file_names_subset)
+		all_kmers = itertools.chain.from_iterable(sketch._kmers for sketch in sketchs)
+		print("forming the set")
+		all_kmers_set = set(all_kmers)
+		to_write = ""
+		for kmer in all_kmers_set:
+			to_write += ">seq%d\n" % iter
+			to_write += "%s\n" % kmer
+			iter += 1
+		fid.write(to_write)
 
