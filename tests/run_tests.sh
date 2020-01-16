988e08e538241eb44b3e2536c631377e1fe326d7
dkoslicki <dmkoslicki@gmail.com>
2020-01-16 10:15:58 -0800

    add integration test script. More python3 debugging. #13

---
+++ tests/run_tests.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# For manual tests. Run this in the tests folder
+
+# make the training database
+echo "Training on data"
+rm TrainingDatabase.h5 2> /dev/null
+/usr/bin/time python ../scripts/MakeStreamingDNADatabase.py filenames.txt TrainingDatabase.h5
+if test -f TrainingDatabase.h5; then
+  echo "Training file successfully created"
+fi
+
+
+echo "Classifying sample, default settings"
+rm results.csv 2> /dev/null
+# make a streaming pre-filter
+/usr/bin/time python ../scripts/StreamingQueryDNADatabase.py Organisms/taxid_1192839_4_genomic.fna.gz TrainingDatabase.h5 results.csv 5-21-2
+if test -f results.csv; then
+  echo "default classify successful"
+fi
+
+echo "Classifying sample, verbose settings"
+rm results.csv 2> /dev/null
+# make a streaming pre-filter
+/usr/bin/time python ../scripts/StreamingQueryDNADatabase.py Organisms/taxid_1192839_4_genomic.fna.gz TrainingDatabase.h5 results.csv 5-21-2 -v
+if test -f results.csv; then
+  echo "verbose classify successful"
+fi
+
+echo "Classifying sample, sensitive settings"
+rm results.csv 2> /dev/null
+# make a streaming pre-filter
+/usr/bin/time python ../scripts/StreamingQueryDNADatabase.py Organisms/taxid_1192839_4_genomic.fna.gz TrainingDatabase.h5 results.csv 5-21-2 --sensitive
+if test -f results.csv; then
+  echo "sensitive classify successful"
+fi
+
+echo "Create streaming prefilter"
+rm prefilter.bf 2> /dev/null
+# make a streaming pre-filter
+/usr/bin/time python ../scripts/MakeStreamingPrefilter.py TrainingDatabase.h5 prefilter.bf 5-21-2
+if test -f prefilter.bf; then
+  echo "streaming prefilter creation successful"
+fi
+
+echo "Classifying sample, default settings with prefilter"
+rm results.csv 2> /dev/null
+# make a streaming pre-filter
+/usr/bin/time python ../scripts/StreamingQueryDNADatabase.py Organisms/taxid_1192839_4_genomic.fna.gz TrainingDatabase.h5 results.csv 5-21-2 -f prefilter.bf
+if test -f results.csv; then
+  echo "default classify with prefilter successful"
+fi
