8ea4df5da12d261166a6443126a73a7ac47881d3
x-zang <48129449+x-zang@users.noreply.github.com>
2020-02-24 11:35:01 -0500

    save/load MHS counts (with larger sketch size) # 21

--- k_mer_dist/k_mer_dist_test.py
+++ k_mer_dist/k_mer_dist_test.py
@@ -32,9 +32,31 @@ def k_mer_sketch_histogram(n, k, genome, true_histogram=True, histogram_name=Non
     outpath = os.path.dirname(os.path.realpath(__file__)) + '/kmc_global_count/'
     # if the value not stored, compute it, else load it
     if not os.path.isfile(outpath + KMC_outname + '.sketch' + str(n) + '.pickle'):
-        MHS = MH.CountEstimator(n=n, ksize=k, save_kmers='y', input_file_name=genome, rev_comp=rev_comp)
-        # turn array of counts of k-mers into occurence of k-mers with the counts
-        counts = MHS._counts
+        # if MinHash Estimator with larger sketch size doesn't exists, compute it with current sketch size
+        MHS_filenames = os.listdir(outpath + '/MH_counts/')
+        if MHS_filenames:
+            try:
+                # get min sketch sizes of existing MinHash Estimator which is greater than n
+                sketch_size_existing = [int(_.split('.sketch')[-1].split('.MHScounts.pickle')[0]) for _ in MHS_filenames
+                                        if _.endswith('.MHScounts.pickle') and '.ksize' + str(k) + '.' in _]
+                sketch_size_existing_greater_than_n = min([_ for _ in sketch_size_existing if _ >= n])
+                MHS_count_name = outpath + '/MH_counts/' + KMC_outname + '.sketch' + str(sketch_size_existing_greater_than_n) + '.MHScounts.pickle'
+                with open(MHS_count_name, 'rb') as MHS_sketch_count_file:
+                    MHS_count = pickle.load(MHS_sketch_count_file)
+                    counts = MHS_count[:n]
+            # sketch_size_existing_greater_than_n is empty
+            except ValueError:
+                MHS = MH.CountEstimator(n=n, ksize=k, save_kmers='y', input_file_name=genome, rev_comp=rev_comp)
+                counts = MHS._counts
+        else:
+            MHS = MH.CountEstimator(n=n, ksize=k, save_kmers='y', input_file_name=genome, rev_comp=rev_comp)
+            counts = MHS._counts
+        # check if MHS counts with k & n is saved nor not
+        MHS_count_name = outpath + '/MH_counts/' + KMC_outname + '.sketch' + str(n) + '.MHScounts.pickle'
+        if not os.path.isfile(MHS_count_name):
+            with open(MHS_count_name, 'wb') as MHS_sketch_count_file:
+                pickle.dump(counts, MHS_sketch_count_file)
+        # turn array of counts of k-mers into occurrence of k-mers with the counts
         dist = np.zeros(max(counts))
         for _c in counts:
             dist[_c - 1] = dist[_c - 1] + 1
