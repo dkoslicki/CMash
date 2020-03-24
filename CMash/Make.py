585565663e62148a7ca70f977a15930d7b2cbf4e
dkoslicki <dmkoslicki@gmail.com>
2020-03-24 12:29:50 -0700

    add old method of making TST #29

--- CMash/Make.py
+++ CMash/Make.py
@@ -8,7 +8,7 @@ import h5py
 
 class MakeTSTNew:
     def __init__(self, training_database_file_name: str, TST_export_file_name: str):
-        self. training_database_file_name = training_database_file_name
+        self.training_database_file_name = training_database_file_name
         self.TST_export_file_name = TST_export_file_name
 
     @staticmethod
@@ -38,6 +38,7 @@ class MakeTSTNew:
                 temp_kmers = subgrp["kmers"][...]
                 kmers = [kmer.decode('utf-8') for kmer in temp_kmers]
                 for (kmer_index, kmer) in enumerate(kmers):
+                    # add both the original k-mer and the reverse complement, as the MinHashes were created without reverse complement
                     if kmer:
                         yield kmer + 'x' + str(i) + 'x' + str(kmer_index)  # format here is kmer+x+hash_index+kmer_index
                         # rev-comp kmer
@@ -45,5 +46,33 @@ class MakeTSTNew:
                         yield kmer_rc + 'x' + str(i) + 'x' + str(kmer_index)  # format here is kmer+x+hash_index+kmer_index
 
     def make_TST(self):
-        tree = mt.Trie(self.yield_trie_items_to_insert_no_import(self. training_database_file_name))
+        tree = mt.Trie(self.yield_trie_items_to_insert_no_import(self.training_database_file_name))
+        tree.save(self.TST_export_file_name)
+
+
+class MakeTSTOld:
+    def __init__(self, genome_sketches: list, TST_export_file_name: str):
+        self.genome_sketches = genome_sketches
+        self.TST_export_file_name = TST_export_file_name
+
+    @classmethod
+    def make_TST(self):
+        genome_sketches = self.genome_sketches
+        to_insert = set()
+        # add both the original k-mer and the reverse complement, as the MinHashes were created without reverse complement
+        for i in range(len(genome_sketches)):
+            for kmer_index in range(len(genome_sketches[i]._kmers)):
+                # normal kmer
+                kmer = genome_sketches[i]._kmers[kmer_index]
+                # only insert the kmer if it's actually non-empty
+                if kmer:
+                    to_insert.add(
+                        kmer + 'x' + str(i) + 'x' + str(kmer_index))  # format here is kmer+x+hash_index+kmer_index
+                    # rev-comp kmer
+                    kmer = khmer.reverse_complement(genome_sketches[i]._kmers[kmer_index])
+                    to_insert.add(
+                        kmer + 'x' + str(i) + 'x' + str(kmer_index))  # format here is kmer+x+hash_index+kmer_index
+
+        # export the TST
+        tree = mt.Trie(to_insert)
         tree.save(self.TST_export_file_name)
